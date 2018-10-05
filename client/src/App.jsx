cb37dcb249226a3493f785c13edcd9290d667bae
abimaelhernandez <hernandezabimael364@gmail.com>
2018-10-05 13:09:01 -0500

    branch created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import React,{Component} from "react";
 export default class App extends Component {
   render(){
     return (
-      <div><h1> Hello starting project ODA </h1></div>
+      <div><h1> Hello starting project ODA  in here</h1></div>
     )
   }
 }
